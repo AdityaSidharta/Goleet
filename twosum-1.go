62af105839f406462191a4576bad51af2400d518
Aditya Sidharta <aditya.sdrt@gmail.com>
2019-05-15 09:56:36 +0800

    First Commit

---
+++ twosum-1.go
@@ -0,0 +1,20 @@
+package goleet
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+    mymap := make(map[int]int)
+    for key1, num1 := range nums {
+        mymap[target - num1] = key1
+    }
+    fmt.Println(mymap)
+    for key2, num2 := range nums {
+        key1, ok := mymap[num2]
+        if ok{
+            if key1 != key2 {
+                return []int{key2, key1}
+            }
+        }
+    }
+    return []int{0, 1}
+}
